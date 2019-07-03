79ff611117395f657beaf4914890d8e5aa1295f5
yunser <>
2019-07-04 01:35:49 +0800

    backu[

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/AppBar'
+import { Button } from "yunser-ui-react"
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static" color="default">
+      </AppBar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        物理
+        <Button text={'haha'}></Button>
       </header>
     </div>
   );
---
+++ src/components/AppBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+// console.log('styles', styles)
+
+export default function AppBar() {
+    return (
+        <div className="ui-app-bar">121212</div>
+    )
+}
